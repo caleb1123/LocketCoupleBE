d892193a7eb6a204aae911c1ee6482f4c9be609c
Dolakien <ngomeo098@gmail.com>
2024-10-27 13:46:44 +0700

    add message service

--- src/main/java/com/fpt/locketcoupleapi/service/MessageService.java
+++ src/main/java/com/fpt/locketcoupleapi/service/MessageService.java
@@ -1,4 +1,17 @@
 package com.fpt.locketcoupleapi.service;
 
+import com.fpt.locketcoupleapi.entity.Message;
+import com.fpt.locketcoupleapi.payload.DTO.MessageDTO;
+
 public interface MessageService {
+    MessageDTO getMessageByPhoto(int photoId);
+
+    MessageDTO getAllMessage();
+
+    MessageDTO createMessage(MessageDTO messageDTO);
+
+    void deleteMessage(int messageId);
+
+    MessageDTO updateMessage(int messageId);
+
 }
--- src/main/java/com/fpt/locketcoupleapi/service/impl/MessageServiceImpl.java
+++ src/main/java/com/fpt/locketcoupleapi/service/impl/MessageServiceImpl.java
@@ -1,5 +1,6 @@
 package com.fpt.locketcoupleapi.service.impl;
 
+import com.fpt.locketcoupleapi.payload.DTO.MessageDTO;
 import com.fpt.locketcoupleapi.repository.MessageRepository;
 import com.fpt.locketcoupleapi.service.MessageService;
 import com.nimbusds.oauth2.sdk.Message;
@@ -16,4 +17,28 @@ public class MessageServiceImpl implements MessageService {
     ModelMapper mapper;
 
 
+    @Override
+    public MessageDTO getMessageByPhoto(int photoId) {
+        return null;
+    }
+
+    @Override
+    public MessageDTO getAllMessage() {
+        return null;
+    }
+
+    @Override
+    public MessageDTO createMessage(MessageDTO messageDTO) {
+        return null;
+    }
+
+    @Override
+    public void deleteMessage(int messageId) {
+
+    }
+
+    @Override
+    public MessageDTO updateMessage(int messageId) {
+        return null;
+    }
 }
